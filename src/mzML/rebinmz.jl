d11d2b5f0bff6fd6c9da4f32c981990c3531fafe
Hanqing <hatchingideas@gmail.com>
2018-08-20 08:46:28 +0100

    Minor modifications.

--- src/mzML/rebinmz.jl
+++ src/mzML/rebinmz.jl
@@ -1,7 +1,20 @@
 function rebinmz(MZ :: Vector, Intensity :: Vector, LinMZ_IParam :: InterpParam)
 
+    StartVal = getstartval(LinMZ_IParam)
+    EndVal = getendval(LinMZ_IParam)
+    Res = getres(LinMZ_IParam)
+
+    (StartVal < minimum(MZ)) && (EndVal > maximum(MZ)) && (StartVal < EndVal) ? nothing : throw(ErrorException("Wrong MZ range. "))
+    MZRange = collect(StartVal : Res : EndVal)
+
     LinIntensity = zeros(eltype(Intensity), length(Intensity))
 
-    getres(LinMZ_IParam)
+    for i in 1:length(MZ)
+
+        LinIntensity[start(searchsorted(MZRange, MZ[i]))] += Intensity[i]
+
+    end
+
+    LinIntensity
 
 end
