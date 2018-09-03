a5793542d634205d16a5299e3e738bad6cb0fcbe
hanqing <hatchingideas@gmail.com>
2018-09-03 08:48:48 +0100

    Modified interpolation to remove the boundary conditions.

--- src/BSplInterp/bspl_interp_derivative.jl
+++ src/BSplInterp/bspl_interp_derivative.jl
@@ -36,43 +36,17 @@ function bspl_interp_derivative(RTAdjVec :: Vector, IMG :: Matrix)
     IMGInterp = zeros(eltype(IMG), RTLen, MZLen)
     IMGDer = zeros(eltype(IMG), RTLen, MZLen)
 
-    # Boundary cases:
-    InterpIdx = New_RT_Idx_On_Grid[1]:(New_RT_Idx_On_Grid[2]-1)
-    u = (RTVec[InterpIdx] .- NewRTVec[1])/(New_RT_Idx_On_Grid[2] - New_RT_Idx_On_Grid[1])
-    InterpIdx = New_RT_Idx_On_Grid[1]:(New_RT_Idx_On_Grid[2] - 1)
-    NormBU = norm([BU[3].(u); BU[2].(u); BU[1].(u)], 1)
-    IMGInterp[InterpIdx, :] += (BU[3].(u) * NewIMG_CP[1,:]' .+ BU[2].(u) * NewIMG_CP[2,:]' .+ BU[1].(u) * NewIMG_CP[3,:]') / NormBU
-    IMGDer[InterpIdx, :] += (DBU[3].(u) * NewIMG_CP[1,:]' .+ DBU[2].(u) * NewIMG_CP[2,:]' .+ DBU[1].(u) * NewIMG_CP[3,:]') / NormBU
-
-    InterpIdx = New_RT_Idx_On_Grid[end-1]:(New_RT_Idx_On_Grid[end])
-    u = (RTVec[InterpIdx] .- NewRTVec[end-1])/(New_RT_Idx_On_Grid[end] - New_RT_Idx_On_Grid[end-1])
-    NormBU = norm([BU[4].(u); BU[3].(u); BU[2].(u)], 1)
-    IMGInterp[InterpIdx,:] += (BU[4].(u) * NewIMG_CP[end-2,:]' .+ BU[3].(u) * NewIMG_CP[end-1,:]' .+ BU[2].(u) * NewIMG_CP[end,:]') / NormBU
-    IMGDer[InterpIdx,:] += (DBU[4].(u) * NewIMG_CP[end-2,:]' .+ DBU[3].(u) * NewIMG_CP[end-1,:]' .+ DBU[2].(u) * NewIMG_CP[end,:]') / NormBU
-
-    for i in 2 : (NumUniqueCP - 2)
+    # No boundary cases: assuming at least five rows of zeros along each boundary.
+    for i in 3 : (NumUniqueCP - 1)
 
         InterpIdx = New_RT_Idx_On_Grid[i]:(New_RT_Idx_On_Grid[i+1] - 1)
         u = (RTVec[InterpIdx] .- NewRTVec[i])/(New_RT_Idx_On_Grid[i+1] - New_RT_Idx_On_Grid[i])
         NormBU = norm([BU[4].(u); BU[3].(u); BU[2].(u); BU[1].(u)], 1)
-        IMGInterp[InterpIdx,:] += (BU[4].(u) * NewIMG_CP[i-1,:]' .+ BU[3].(u) * NewIMG_CP[i,:]' .+ BU[2].(u) * NewIMG_CP[i+1,:]' + BU[1].(u) * NewIMG_CP[i+2,:]') / NormBU
-        IMGDer[InterpIdx,:] += (DBU[4].(u) * NewIMG_CP[i-1,:]' .+ DBU[3].(u) * NewIMG_CP[i,:]' .+ DBU[2].(u) * NewIMG_CP[i+1,:]' + DBU[1].(u) * NewIMG_CP[i+2,:]') / NormBU
+        IMGInterp[InterpIdx,:] += (BU[4].(u) * NewIMG_CP[i-2,:]' .+ BU[3].(u) * NewIMG_CP[i-1,:]' .+ BU[2].(u) * NewIMG_CP[i,:]' + BU[1].(u) * NewIMG_CP[i+1,:]') / NormBU
+        IMGDer[InterpIdx,:] += (DBU[4].(u) * NewIMG_CP[i-2,:]' .+ DBU[3].(u) * NewIMG_CP[i-1,:]' .+ DBU[2].(u) * NewIMG_CP[i,:]' + DBU[1].(u) * NewIMG_CP[i+1,:]') / NormBU
 
     end
 
     (IMGInterp, IMGDer)
 
 end
-
-IMG = ones(10, 10)
-[IMG[i,:] = i*IMG[i,:] for i in 1:10]
-IMG[[1,2,9,10],:] = 0
-
-IMG
-
-
-RTAdj = -ones(10)
-
-IMG
-
-(I, D) = bspl_interp_derivative(RTAdj,IMG)
